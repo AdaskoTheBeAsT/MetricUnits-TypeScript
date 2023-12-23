73e79c13210736405337222f09fcc32753440aee
Adam Pluciński <adaskothebeast@gmail.com>
2023-12-23 23:51:42 +0100

    publish cjs and mjs

--- publish.bat
+++ publish.bat
@@ -1,4 +1,5 @@
 xcopy .\LICENSE .\dist\libs\metric-units\ /Y
 xcopy .\README.md .\dist\libs\metric-units\ /Y
+node .\tools\scripts\cleanPackage.mjs
 cd dist/libs/metric-units
 REM npm publish --tag=latest --access public
